20b746fed976670d4fb42aee9b003142c199d830
nmbenzo <nmbenzo@gmail.com>
2018-10-30 10:52:19 -0700

    Revert "Delete border_formatting.py"
    This reverts commit 2e8a21e

---
+++ border_formatting.py
@@ -0,0 +1,7 @@
+from openpyxl.styles.borders import Border, Side
+
+
+thin_border = Border(left=Side(style='thin'),
+                     right=Side(style='thin'),
+                     top=Side(style='thin'),
+                     bottom=Side(style='thin'))
