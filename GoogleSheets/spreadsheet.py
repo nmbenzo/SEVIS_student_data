3f075df2c4d43eb33cfa236f4d7ce1cae62aae6d
nmbenzo <nmbenzo@gmail.com>
2018-11-12 22:29:43 -0800

    Added initial gspread cell update functionality

--- GoogleSheets/spreadsheet.py
+++ GoogleSheets/spreadsheet.py
@@ -1,18 +1,46 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from datetime import date, datetime
+import openpyxl
 
 
-# use creds to create a client to interact with the Google Drive API
-scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-client = gspread.authorize(creds)
+def authenticate_google_docs():
+    # use creds to create a client to interact with the Google Drive API
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    gc = gspread.authorize(creds)
+    return gc
+
+wb2 = openpyxl.load_workbook('/Users/nbenzschawel/Downloads/SEVIS_raw_data.xlsx') # workbook from SEVIS RTI
+ws2 = wb2.active
+
+
+gc = authenticate_google_docs()
+
+sh = gc.open('201920 SEVIS Registration')
+worksheet_list = sh.worksheets()
 
 
 # Find a workbook by name and open a particular sheet
 # Make sure you use the right name here.
-COL = client.open("201920 SEVIS Registration").sheet1
-transferUG = client.open("201920 SEVIS Registration").sheet2
-transferGR = client.open("201920 SEVIS Registration").sheet3
-new_students = client.open("201920 SEVIS Registration").sheet4
-continuing_students = client.open("201920 SEVIS Registration").sheet5
+
+COL = sh.worksheet('COL')
+transferUG = sh.worksheet('Transfer UG')
+transferGR = sh.worksheet('Transfer GR')
+new_students = sh.worksheet('New Students')
+continuing_students = sh.worksheet('Continuing Students')
+
+
+def update_new_students(ws2):
+    max_row = ws2.max_row
+    max_column = ws2.max_column
+    for rowNum in range(1, max_row+1):
+        for colNum in range(1, max_column+1):
+            cell_value = ws2.cell(row=rowNum, column=colNum).value
+            if isinstance(cell_value, (datetime, date)):
+                new_students.update_cell(row=rowNum, col=colNum, value=cell_value.isoformat())
+            else:
+                new_students.update_cell(row=rowNum, col=colNum, value=cell_value)
+
+update_new_students(ws2)
