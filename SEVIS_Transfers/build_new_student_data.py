8c6b7b6cba7e7cde3384125d1f81da0d5441f2a0
nmbenzo <nmbenzo@gmail.com>
2018-12-03 20:40:46 -0800

    Refractoring and updates to existing code

--- SEVIS_Transfers/build_new_student_data.py
+++ SEVIS_Transfers/build_new_student_data.py
@@ -8,7 +8,16 @@ wb = openpyxl.load_workbook(file1)
 wb2 = openpyxl.load_workbook(file2)
 old_sheet = wb.worksheets[0]
 new_sheet = wb2.worksheets[1]
+final_sheet = wb2.worksheets[0]
 
-max_row = old_sheet.max_row
-max_col = old_sheet.max_column
+row_max_old = old_sheet.max_row
+col_max_old = old_sheet.max_column
 
+row_max_current = new_sheet.max_row
+col_max_current = new_sheet.max_column
+
+row_max_final = final_sheet.max_row
+col_max_final = final_sheet.max_column
+
+
+# pasteRange(1, new_sheet.max_row+1, new_sheet.max_column, rowmax_new, new_sheet, selectedRange)
--- SEVIS_Transfers/build_transfer_data.py
+++ SEVIS_Transfers/build_transfer_data.py
@@ -1,8 +1,8 @@
-import openpyxl
-from SEVIS_Transfers.build_new_student_data import file1, file2, wb, wb2, old_sheet, new_sheet, max_row, max_col
+from SEVIS_Transfers.build_new_student_data import file1, file2, wb, \
+    wb2, old_sheet, new_sheet, row_max_old, col_max_old, final_sheet, row_max_current, col_max_current
 
 
-def copyRange(startCol, startRow, endCol, endRow, old_sheet):
+def copy_new_Range(startCol, startRow, endCol, endRow, old_sheet):
     """
     Copies a range of cells as a nested list
     Takes: start cell, end cell, and the sheet we want to copy from
@@ -17,7 +17,7 @@ def copyRange(startCol, startRow, endCol, endRow, old_sheet):
     return rangeSelected
 
 
-def pasteRange(startCol, startRow, endCol, endRow, sheetReceiving, copiedData):
+def paste_new_Range(startCol, startRow, endCol, endRow, sheetReceiving, copiedData):
     countRow = 0
     for i in range(startRow, endRow+1, 1):
         countCol = 0
@@ -27,16 +27,64 @@ def pasteRange(startCol, startRow, endCol, endRow, sheetReceiving, copiedData):
         countRow += 1
 
 
-def createData():
+def create_new_Data():
+    """Pastes copied range to new sheet which needs to be updates"""
+    print(f'Old Row Range = {new_sheet.max_row}')
     print('Processing data...')
-    selectedRange = copyRange(1, 1, max_col, max_row, old_sheet)
-    pastingRange = pasteRange(1, 1, max_col, max_row, new_sheet, selectedRange)
-
+    if new_sheet.cell(row=1, column=1).value is None:
+        selectedRange = copy_new_Range(1, 1, col_max_old, row_max_old, old_sheet)
+        paste_new_Range(1, (new_sheet.max_row + 1), col_max_old,
+                        (new_sheet.max_row + row_max_old), new_sheet, selectedRange)
+        new_sheet.delete_rows(1)
+    elif new_sheet.cell(row=1, column=1).value == 'SEVIS ID':
+        selectedRange = copy_new_Range(1, 2, col_max_old, row_max_old, old_sheet)
+        paste_new_Range(1, (new_sheet.max_row+1), col_max_old, (new_sheet.max_row + row_max_old-1), new_sheet, selectedRange)
+    else:
+        selectedRange = copy_new_Range(1, 1, col_max_old, row_max_old, old_sheet)
+        paste_new_Range(1, (new_sheet.max_row + 1), col_max_old,
+                   (new_sheet.max_row + row_max_old), new_sheet, selectedRange)
     wb2.save(file2)
     print("Range copied and pasted")
+    print(f'New Row Range = {new_sheet.max_row}')
 
 
-createData()
+def copy_updated_Range(startCol, startRow, endCol, endRow, new_sheet):
+    """
+    Copies a range of cells as a nested list
+    Takes: start cell, end cell, and the sheet we want to copy from
+    """
+    rangeSelected = []
+    for i in range(startRow, endRow+1, 1):
+        rowSelected = []
+        for j in range(startCol, endCol + 1, 1):
+            rowSelected.append(new_sheet.cell(row = i, column = j).value)
+        rangeSelected.append(rowSelected)
+
+    return rangeSelected
 
 
+def paste_updated_Range(startCol, startRow, endCol, endRow, sheetReceiving, copiedData):
+    countRow = 0
+    for i in range(startRow, endRow+1, 1):
+        countCol = 0
+        for j in range(startCol, endCol+1, 1):
+            sheetReceiving.cell(row=i, column=j).value = copiedData[countRow][countCol]
+            countCol += 1
+        countRow += 1
+
+
+def create_updated_Data():
+    """Pastes copied range from new sheet to final sheet - do this after sorting data"""
+    print(f'Current Row Range = {new_sheet.max_row}')
+    print('Processing data...')
+    selectedRange = copy_updated_Range(1, 1, col_max_current, row_max_current, new_sheet)
+    paste_updated_Range(1, (final_sheet.max_row + 1), col_max_current,
+                        (final_sheet.max_row + row_max_current),
+                        final_sheet, selectedRange)
+    wb2.save(file2)
+    print("Range copied and pasted")
+    print(f'New Row Range = {final_sheet.max_row}')
+
+
+create_updated_Data()
 
