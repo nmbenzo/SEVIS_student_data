9894385b64b791553ee1f8cb39d51997fec755dd
nmbenzo <nmbenzo@gmail.com>
2018-12-04 13:25:36 -0800

    Refractoring

--- SEVIS_Transfers/build_transfer_data.py
+++ SEVIS_Transfers/build_transfer_data.py
@@ -32,10 +32,10 @@ def create_new_Data():
     """Pastes copied range to new sheet which needs to be updates"""
     print(f'Old Row Range = {new_sheet.max_row}')
     print('Processing data...')
+
     if new_sheet.cell(row=1, column=1).value is None:
         selectedRange = copy_new_Range(1, 1, col_max_old, row_max_old, old_sheet)
         paste_new_Range(1, 1, col_max_old, row_max_old, new_sheet, selectedRange)
-
 
     elif new_sheet.cell(row=1, column=1).value == 'SEVIS ID':
         selectedRange = copy_new_Range(1, 2, col_max_old, row_max_old, old_sheet)
