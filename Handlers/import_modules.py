d598684bc344b90b226435c11837d88ff1905f2c
nmbenzo <nmbenzo@gmail.com>
2019-01-04 22:14:41 -0800

    Added file handling

--- Handlers/import_modules.py
+++ Handlers/import_modules.py
@@ -1,16 +1,18 @@
-from SEVIS_grad_data.build_grad_data import ws_grad, campusID_SEVISID, \
+from SEVIS_Graduated_Data.build_grad_data import campusID_SEVISID, \
     campusID_work_auth, campusID_end_date, campusID_workend, campusID_emails
 from Handlers.major_advisor_data import master_deg_adv, bachelor_deg_adv
-from SEVIS_grad_data.build_graduated_student_data import add_advisor, \
+from SEVIS_Graduated_Data.build_graduated_student_data import add_advisor, \
     match_SEVISID, add_work_type, add_emails, add_work_enddate, \
     add_profile_enddate
 
-from Compeleted_student_cleaning import build_comp_data, populate_data
+from Compeleted_Student_Cleaning import build_comp_data, populate_data
+from Compeleted_Student_Cleaning.populate_data import match_SEVISID_completed
+from Compeleted_Student_Cleaning.build_comp_data import SEVISID_completed
 
-from Active_students_requiring_registration import build_data, maj_adv_data, populate_data
-from Active_students_requiring_registration.populate_data import active_match_SEVISID, \
+from Active_Students_Requiring_Registration import build_data, maj_adv_data, populate_data
+from Active_Students_Requiring_Registration.populate_data import active_match_SEVISID, \
 match_major_data, match_advisor, match_units
-from Active_students_requiring_registration.build_data import active_campusID_SEVISID, active_SEVISID_major, active_SEVISID_units
+from Active_Students_Requiring_Registration.build_data import active_campusID_SEVISID, active_SEVISID_major, active_SEVISID_units
 from Handlers.major_advisor_data import advisor_major_ug, advisor_major_gr
 
 from New_Student_Registration import build_student_data, build_new_student_advisor_notes
@@ -28,7 +30,10 @@ from SEVIS_Transfers.build_transfer_data import copy_new_Range, paste_new_Range,
     create_new_Data, paste_to_final, grab_final_data, check_in_fsa, find_in_fsa
 from SEVIS_Transfers.create_workbook import create_workbook
 
-from file_management.merge_files import merge_all_workbooks
+from Student_Cancellations.build_initial_cancel_list import copy_new_Range, paste_new_Range, \
+create_new_Data
+
+from File_Management.merge_files import merge_all_workbooks
 
 
 from Google_Drive.G_drive import *
