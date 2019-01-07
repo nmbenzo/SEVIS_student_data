a63f0f6468db16c7b1eae5b0c5a509cd8742a7ea
nmbenzo <nmbenzo@gmail.com>
2019-01-07 12:30:00 -0800

    Added Data Sorting

---
+++ New_Student_Registration/sort_new_data.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from Handlers.file_imports import NEW_students_FINAL, NEW_students
+
+
+def sort_new_data():
+    new_data = pd.read_excel(NEW_students_FINAL, sheet_name=0, index_col=0)
+    sorted_by_advisor = new_data.sort_values(['Advisor'], ascending=True)
+
+    writer = pd.ExcelWriter(NEW_students_FINAL, engine='openpyxl')
+    writer.book = NEW_students
+    writer.sheets = dict((ws.title, ws) for ws in NEW_students.worksheets)
+
+    sorted_by_advisor.to_excel(writer, 'Sheet')
+    writer.save()
+    print('Data sorted by Advisor')
