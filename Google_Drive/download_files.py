c16dc70d43f57e2838fd0f7be4e1a35c9375c40e
nmbenzo <nmbenzo@gmail.com>
2019-01-11 12:09:14 -0800

    Refractoring

--- Google_Drive/download_files.py
+++ Google_Drive/download_files.py
@@ -1,18 +1,11 @@
 from __future__ import print_function
 import io
+from googleapiclient import discovery
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 from googleapiclient.http import MediaIoBaseDownload
-
-
-# If modifying these scopes, delete the file token.json.
-SCOPES = 'https://www.googleapis.com/auth/drive'
-
-# The ID and range of a sample spreadsheet.
-file_id = '1MGAT36t17ZluulYx698lPA26lmfqcHQNFUoF_cM_O9w'
-Excel = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-file_name = 'SEVIS Registration'
+from Handlers.file_imports import file_id, td_id, SCOPES, Excel, file_name
 
 
 def main():
@@ -29,10 +22,24 @@ def main():
         creds = tools.run_flow(flow, store)
     service = build('drive', 'v3', http=creds.authorize(Http()))
 
+    meta = service.files().get(fileId=file_id, fields="*",
+                               supportsTeamDrives=True).execute()
+
+    print(meta)
+
+
+def download_file(file_id, mimeType, file_name):
+    store = file.Storage('credentials.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('client_secrets.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('drive', 'v3', http=creds.authorize(Http()))
+
     # Call the Drive v3 API
     results = service.files().list(
         pageSize=50, supportsTeamDrives=True, includeTeamDriveItems=True,
-        corpora='teamDrive', teamDriveId='0ADUIPThXplYvUk9PVA',
+        corpora='teamDrive', teamDriveId=td_id,
         fields="nextPageToken, files(id, name)").execute()
     items = results.get('files', [])
 
@@ -40,22 +47,32 @@ def main():
         print('No files found.')
     else:
         print('Files:')
-        for item in items:
-            print(u'{0} ({1})'.format(item['name'], item['id']))
 
+    if 'google-apps' in mimeType:
+        return
 
-    meta = service.files().get(fileId=file_id, fields="*",
-                                    supportsTeamDrives=True).execute()
-
-    data = service.files().export(fileId=file_id, mimeType=Excel)
-    fh = io.BytesIO()
+    data = service.files().export_media(fileId=file_id, mimeType=Excel)
+    fh = io.FileIO(file_name, 'wb')
     downloader = MediaIoBaseDownload(fh, data)
     done = False
     while done is False:
         status, done = downloader.next_chunk()
         print("Download: {}".format(int(status.progress() * 100)))
 
-    print(meta)
 
+if __name__ == '__main__':
+    main()
+    store = file.Storage('credentials.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('client_secrets.json', SCOPES)
+        creds = tools.run_flow(flow, store)
 
-main()
+    drive_service = discovery.build('drive', 'v3', http=creds.authorize(Http()))
+    files = drive_service.files().list(pageSize=50, supportsTeamDrives=True, includeTeamDriveItems=True,
+        corpora='teamDrive', teamDriveId=td_id,
+        fields="nextPageToken, files(id, name)").execute()
+    for f in files['files']:
+        if f['id'] == file_id:
+            print(f['name'], f['id'])
+            download_file(file_id, Excel, file_name)
