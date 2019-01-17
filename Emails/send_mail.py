e662c210afce5397ddc39aa43c1e2a91edf5a54d
nmbenzo <nmbenzo@gmail.com>
2019-01-16 21:35:43 -0800

    Added Gmail API Integration

--- Emails/send_mail.py
+++ Emails/send_mail.py
@@ -30,9 +30,8 @@ class SendEmail:
         message['subject'] = subject
 
         message_as_bytes = message.as_bytes()  # the message should converted from string to bytes.
-        message_as_base64 = base64.urlsafe_b64encode(
-            message_as_bytes)  # encode in base64 (printable letters coding)
-        raw = message_as_base64.decode()  # need to JSON serializable (no idea what does it means)
+        message_as_base64 = base64.urlsafe_b64encode(message_as_bytes) # encode in base64 (printable letters coding)
+        raw = message_as_base64.decode()  # need to JSON serializable
         return {'raw': raw}
 
     def create_message_with_attachment(self,
