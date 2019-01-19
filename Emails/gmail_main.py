ddefdd9f709d470b5f989f87b7fac3d2643350a8
nmbenzo <nmbenzo@gmail.com>
2019-01-18 18:51:28 -0800

    Refractoring

--- Emails/gmail_main.py
+++ Emails/gmail_main.py
@@ -2,7 +2,6 @@ from Emails.Gmail_API_Handlers import *
 from Emails.email_content import Header_901, test_file
 
 
-
 def mainEmail():
     sendInstance = send_mail.SendEmail(service)
     message = sendInstance.create_message('nbenzschawel@usfca.edu','nbenzschawel@usfca.edu', 'Final Python Application', test_file)
